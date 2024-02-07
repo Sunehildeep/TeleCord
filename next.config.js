f42bb2567afd01047593fedd89c4b55776c9aa21
GarvChhokra <garvchhokra@gmail.com>
2024-02-07 02:53:49 -0500

    structure for header

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   i18n,
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname);
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: path.join(__dirname, 'public', 'locales'),
-            to: path.join(__dirname, 'static', 'locales'),
-          },
-        ],
-      })
-    )
+    // config.plugins.push(
+    //   new CopyWebpackPlugin({
+    //     patterns: [
+    //       {
+    //         from: path.join(__dirname, 'public', 'locales'),
+    //         to: path.join(__dirname, 'static', 'locales'),
+    //       },
+    //     ],
+    //   })
+    // )
     return config;
   },
 };
