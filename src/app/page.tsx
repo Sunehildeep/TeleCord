ffa37690cffa984543ec63d5d67b8137ba5b1d52
Sunehildeep <sunehildeepsinghsandhu123@gmail.com>
2024-02-07 16:04:22 -0500

    File structure updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-"use client"
-import React from 'react';
-import PageTitle from '@/components/common/page-title';
-import { useTranslation } from 'react-i18next';
-import Image from 'next/image';
+"use client";
+import React from "react";
+import PageTitle from "@/components/PageTitle";
+import { useTranslation } from "react-i18next";
+import Image from "next/image";
 
 export default function Home() {
-  const { t } = useTranslation();
-  return (
-    <>
-      <PageTitle title="Telecord" />
-      <h1>{t('title')}</h1>
-    </>
-  );
+	const { t } = useTranslation();
+	return (
+		<>
+			<PageTitle title="Telecord" />
+			<h1>{t("title")}</h1>
+		</>
+	);
 }
