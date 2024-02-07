06ad119ed3daf825d11f4e9fcc72ee73d5adb669
Sunehildeep <sunehildeepsinghsandhu123@gmail.com>
2024-02-07 16:48:21 -0500

    Translation Set-up

--- next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-const { i18n } = require('next-i18next');
-
-module.exports = {
-  i18n,
-  locales: ['en', 'fr'],
-  defaultLocale: 'en',
-  localeDetection: false,
-};
--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-const path = require('path');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { i18n } = require('./next-i18next.config');
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-module.exports = {
-  i18n,
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    // config.plugins.push(
-    //   new CopyWebpackPlugin({
-    //     patterns: [
-    //       {
-    //         from: path.join(__dirname, 'public', 'locales'),
-    //         to: path.join(__dirname, 'static', 'locales'),
-    //       },
-    //     ],
-    //   })
-    // )
-    return config;
-  },
-};
+module.exports = nextConfig;
